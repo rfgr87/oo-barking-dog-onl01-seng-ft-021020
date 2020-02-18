28124e8136ea1abfdbf32b51d4e09e01b18b21f8
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-18 15:46:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 # Your code goes here!
 class Dog 
-  def name= (@)
+  def name= (dog_name)
+    @this_dogs_name = dog_name
+  end
+  
+  def name
+    puts dog_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end
